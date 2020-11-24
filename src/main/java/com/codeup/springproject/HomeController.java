8d1cea04d5c7bb9f55791208c103998214608dce
Cory <courtneynholley@gmail.com>
2020-11-24 09:51:28 -0600

    FEAT: SPRING CONTROLLERS EXERCISE

---
+++ src/main/java/com/codeup/springproject/HomeController.java
@@ -0,0 +1,15 @@
+package com.codeup.springproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String landing() {
+        return "This is the landing page!";
+    }
+}
---
+++ src/main/java/com/codeup/springproject/PostController.java
@@ -0,0 +1,33 @@
+package com.codeup.springproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String index() {
+        return "Posts index page";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String individualPost(@PathVariable int id) {
+        return "View an individual post";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String viewCreateForm() {
+        return "View the form for creating a post";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String createPost() {
+        return "Create a new post";
+    }
+
+}
