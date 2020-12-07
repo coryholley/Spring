80c764bf2f6bbb565204d47e5ae03434ae161724
Cory <courtneynholley@gmail.com>
2020-12-07 16:25:19 -0600

    FEAT: Added css & js docs, added a dynamic image input field on create page using javascript

--- src/main/resources/static/js/blog-project.js
+++ src/main/resources/static/js/blog-project.js
@@ -1,13 +1,35 @@
 (function() {
     "use strict";
-    //TEST
-    // var listener = function (event) {
-    //     alert('You clicked the button!');
-    // }
-    //
-    // // register the listener to handle clicks on btn1
-    // document.getElementById('addMoreImages').addEventListener('click', listener);
 
-        window.onload($('#image-input2').hide());
+    $(document).ready(function() {
+
+        function setAttributes(el, attrs) {
+            for(var key in attrs) {
+                el.setAttribute(key, attrs[key]);
+            }
+        }
+
+        var counter = 1;
+        var limit = 3;
+        function addInput(formImages){
+            if (counter === limit)  {
+                alert("You have reached the limit of adding 3 images");
+            }
+            else {
+                let newInput = document.createElement('input');
+                setAttributes(newInput, {placeholder:"Image Path", name:"image-input" + counter, class:"form-control mb-1", type:"text"})
+                document.getElementById('formImages').appendChild(newInput);
+                counter++;
+            }
+        }
+
+
+        $('#addMoreImages').on('click', function() {
+            addInput()
+        });
+
+
+
+    });
 
 })();
