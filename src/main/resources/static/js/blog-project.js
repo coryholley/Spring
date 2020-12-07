3453aefa5ad9212b98bf74815d707d8b94313d45
Cory <courtneynholley@gmail.com>
2020-12-07 14:35:19 -0600

    Feat:Trying to add custom CSS & JS

---
+++ src/main/resources/static/js/blog-project.js
@@ -0,0 +1,13 @@
+(function() {
+    "use strict";
+    //TEST
+    // var listener = function (event) {
+    //     alert('You clicked the button!');
+    // }
+    //
+    // // register the listener to handle clicks on btn1
+    // document.getElementById('addMoreImages').addEventListener('click', listener);
+
+        window.onload($('#image-input2').hide());
+
+})();
